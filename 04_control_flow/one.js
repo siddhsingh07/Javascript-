4a4a7f3909a24cb0c5b48c63d51823844aa439e8
siddhsingh07 <siddhsingh777@gmail.com>
2024-03-18 23:16:07 +0530

    commit

---
+++ 04_control_flow/one.js
@@ -0,0 +1,19 @@
+// if
+
+const isUserLoggedIn = true 
+
+const temperature = 41
+
+if ( temperature == 41 ) {
+    console.log("less than 50");
+} else {
+    console.log("temperature is above 50");
+}
+
+console.log("execute");
+
+
+
+
+
+// < , > , <= , >= , == , != , === , !==
