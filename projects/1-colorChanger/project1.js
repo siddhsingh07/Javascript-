fb9292759f89bd04cad90fec6aaaa5147aad468d
siddhsingh07 <siddhsingh777@gmail.com>
2024-03-26 21:22:25 +0530

    commit changes

--- projects/1-colorChanger/project1.js
+++ projects/1-colorChanger/project1.js
@@ -3,6 +3,7 @@ console.log("siddh")
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
 
+
 buttons.forEach(function (button) {
     console.log(button);
     button.addEventListener('click', function (e) {
